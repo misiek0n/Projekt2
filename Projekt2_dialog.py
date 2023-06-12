479147cc18ac752066a3fd80b53b0c7692cca693
misiek0n <m.bielecki1909@gmail.com>
2023-06-12 12:22:31 +0200

    jedna galazka

--- Projekt2_dialog.py
+++ Projekt2_dialog.py
@@ -24,9 +24,13 @@
 
 import os
 
+import numpy as np
+import math
+
 from qgis.PyQt import uic
 from qgis.PyQt import QtWidgets
-from qgis.core import Qgis, QgsVectorLayer, QgsGeometry, QgsFeature, QgsField, QgsProject
+from qgis.core import Qgis, QgsVectorLayer, QgsGeometry, QgsFeature, QgsField, QgsProject, QgsPoint, QgsField, QgsFields
+from qgis.core import QgsCoordinateReferenceSystem
 from qgis.utils import iface
 
 
@@ -50,6 +54,7 @@ class Projekt2Dialog(QtWidgets.QDialog, FORM_CLASS):
         self.radioButton_ar.setVisible(False)
         self.radioButton_ha.setVisible(False)
         self.label_jedn_pole.setVisible(False)
+        self.pushButton_porow.setVisible(False)
         self.tableWidget_wybrane.setColumnCount(2)
         self.tableWidget_wybrane.setHorizontalHeaderLabels(['Nr', 'h'])
         self.comboBox_obliczenie.currentIndexChanged.connect(self.update_visibility)
@@ -60,6 +65,7 @@ class Projekt2Dialog(QtWidgets.QDialog, FORM_CLASS):
         self.pushButton_clear.clicked.connect(self.clear_display)
         self.pushButton_rysuj.clicked.connect(self.create_polygon)
         self.pushButton_porow.clicked.connect(self.compare_area)
+        self.pushButton_explore.clicked.connect(self.explore)
         self.mMapLayerComboBox_layers.currentLayer().selectionChanged.connect(self.update_table)
 
     def update_visibility(self):
@@ -70,17 +76,20 @@ class Projekt2Dialog(QtWidgets.QDialog, FORM_CLASS):
             self.radioButton_ha.setVisible(True)
             self.label_jedn_pole.setVisible(True)
             self.pushButton_rysuj.setVisible(True)
+            self.pushButton_porow.setVisible(True)
         else:
             self.radioButton_m2.setVisible(False)
             self.radioButton_ar.setVisible(False)
             self.radioButton_ha.setVisible(False)
             self.label_jedn_pole.setVisible(False)
             self.pushButton_rysuj.setVisible(False)
+            self.pushButton_porow.setVisible(False)
+
     def update_table(self):
         selected_option = self.comboBox_obliczenie.currentText()
         self.tableWidget_wybrane.clearContents()
         self.tableWidget_wybrane.setRowCount(0)
-    
+
         if selected_option == 'Pole powierzchni':
             self.tableWidget_wybrane.setColumnCount(3)
             self.tableWidget_wybrane.setHorizontalHeaderLabels(['Nr', 'X', 'Y'])
@@ -95,23 +104,23 @@ class Projekt2Dialog(QtWidgets.QDialog, FORM_CLASS):
             return
         layer = self.mMapLayerComboBox_layers.currentLayer()
         selected_points = layer.selectedFeatures()
-    
+
         ID = []
         Xcoord = []
         Ycoord = []
-    
+
         for point in selected_points:
             idnumber = point['ID']
             ID.append(idnumber)
-    
+
             xcoord = point['xcoord']
             Xcoord.append(xcoord)
-    
+
             ycoord = point['ycoord']
             Ycoord.append(ycoord)
-    
+
         self.tableWidget_wybrane.setRowCount(len(ID))
-    
+
         for row, data in enumerate(zip(ID, Xcoord, Ycoord)):
             for col, value in enumerate(data):
                 item = QtWidgets.QTableWidgetItem(str(value))
@@ -122,24 +131,24 @@ class Projekt2Dialog(QtWidgets.QDialog, FORM_CLASS):
             return
         layer = self.mMapLayerComboBox_layers.currentLayer()
         selected_points = layer.selectedFeatures()
-    
+
         heights = []
         ID = []
-    
+
         for point in selected_points:
             zcoord = point['zcoord']
             heights.append(zcoord)
-    
+
             idnumber = point['ID']
             ID.append(idnumber)
-    
+
         self.tableWidget_wybrane.setRowCount(len(heights))
-    
+
         for row, data in enumerate(zip(ID, heights)):
             for col, value in enumerate(data):
                 item = QtWidgets.QTableWidgetItem(str(value))
                 self.tableWidget_wybrane.setItem(row, col, item)
-   
+
 #UWAGA
 
     def calculate_height_diff(self):
@@ -158,12 +167,12 @@ class Projekt2Dialog(QtWidgets.QDialog, FORM_CLASS):
             for point in selected_points:
                 zcoord = point.attribute(layer.fields().indexFromName('zcoord'))
                 heights.append(zcoord)
-                
+
             ID = []
             for point in selected_points:
                 idnumber = point.attribute(layer.fields().indexFromName('ID'))
                 ID.append(idnumber)
-                
+
             delta_height = round(heights[1] - heights[0],5)
             self.label_wynik.setText(f"Różnica wysokosći pomiędzy punktem {ID[1]} i punktem {ID[0]} wynosi: {delta_height} [m]")
             iface.messageBar().pushMessage("Sukces",
@@ -186,32 +195,22 @@ class Projekt2Dialog(QtWidgets.QDialog, FORM_CLASS):
                                            "Wybrano zbyt małą liczbę punktów do obliczenia pola powierzchni",
                                            level=Qgis.Critical,
                                            duration=5)
-        else:
-            xcoords = []
-            ycoords = []
-            for point in selected_points:
-                point_xy = point.geometry().asPoint()
-                xcoords.append(point_xy.x())
-                ycoords.append(point_xy.y())
-            field = 0
-            for i in range(0, len(xcoords)):
-                if i < len(xcoords) - 2:
-                    part_field = (xcoords[i+1] + xcoords[i]) * (ycoords[i+1] - ycoords[i])
-                    field += part_field
-                else:
-                    part_field = (xcoords[0] + xcoords[i]) * (ycoords[0] - ycoords[i])
-                    field += part_field
-            if self.radioButton_m2.isChecked():
-                self.label_wynik.setText(f"Wynik to: {field} metrów kwadratowych")
-            elif self.radioButton_ar.isChecked():
-                self.label_wynik.setText(f"Wynik to: {field/ 100} arów")
-            elif self.radioButton_ha.isChecked():
-                self.label_wynik.setText(f"Wynik to: {field / 10000} hektarów")
-            iface.messageBar().pushMessage("Sukces",
-                                           "Obliczanie powiodło się",
-                                           level=Qgis.Success,
-                                           duration=5)
-
+        points = [feature.geometry().asPoint() for feature in selected_points]
+        centroid = QgsPoint(sum(point.x() for point in points) / len(points),
+                            sum(point.y() for point in points) / len(points))
+        points.sort(key=lambda point: -math.atan2(point.y() - centroid.y(), point.x() - centroid.x()))
+        area = 0.5 * np.abs(
+            sum(points[i - 1].x() * points[i].y() - points[i].x() * points[i - 1].y() for i in range(len(points))))
+        if self.radioButton_m2.isChecked():
+            self.label_wynik.setText(f"Wynik to: {area} metrów kwadratowych")
+        elif self.radioButton_ar.isChecked():
+            self.label_wynik.setText(f"Wynik to: {area/ 100} arów")
+        elif self.radioButton_ha.isChecked():
+            self.label_wynik.setText(f"Wynik to: {area/ 10000} hektarów")
+        iface.messageBar().pushMessage("Sukces",
+                                       "Obliczanie powiodło się",
+                                       level=Qgis.Success,
+                                       duration=5)
     def clear_display(self):
         self.label_wynik.setText('')
         self.mMapLayerComboBox_layers.currentLayer().removeSelection()
@@ -219,24 +218,28 @@ class Projekt2Dialog(QtWidgets.QDialog, FORM_CLASS):
     def create_polygon(self):
         canvas = iface.mapCanvas()
         layer = self.mMapLayerComboBox_layers.currentLayer()
+        crs = layer.crs()
+        crs_authid = crs.authid()
         selected_features = layer.selectedFeatures()
         if len(selected_features) < 3:
             iface.messageBar().pushMessage("Błąd",
                                            "Wybierz co najmniej 3 punkty do narysowania poligonu.",
                                            level=Qgis.Warning)
             return
-        polygon = QgsGeometry.fromPolygonXY([[point.geometry().asPoint() for point in selected_features]])
-        new_layer = QgsVectorLayer("Polygon?crs=" + layer.crs().toWkt(), "Poligon", "memory")
+        points = [feature.geometry().asPoint() for feature in selected_features]
+        centroid = QgsPoint(sum(point.x() for point in points) / len(points),
+                            sum(point.y() for point in points) / len(points))
+        points.sort(key=lambda point: -math.atan2(point.y() - centroid.y(), point.x() - centroid.x()))
+        new_layer = QgsVectorLayer("Polygon?crs=" + crs_authid, "Poligon", "memory")
         provider = new_layer.dataProvider()
         new_layer.startEditing()
-        feature = QgsFeature()
-        feature.setGeometry(polygon)
-        provider.addFeatures([feature])
+        poly_feature = QgsFeature()
+        polygon = QgsGeometry.fromPolygonXY([points])
+        poly_feature.setGeometry(polygon)
+        provider.addFeature(poly_feature)
         new_layer.commitChanges()
         QgsProject.instance().addMapLayer(new_layer)
-        layer.removeSelection()
         canvas.refresh()
-# dodac wybor kolejnosci punktów w poligonie
 
     def compare_area(self):
         layer = self.mMapLayerComboBox_layers.currentLayer()
@@ -255,3 +258,7 @@ class Projekt2Dialog(QtWidgets.QDialog, FORM_CLASS):
 
     def clear_porow_label(self):
         self.label_porow.setText('')
+
+    def explore(self):
+        file = QtWidgets.QFileDialog.getOpenFileName(self, 'Open file', "C:\\")
+        self.lineEdit_sciezka.setText(file[0])
--- Projekt2_dialog_base.py
+++ Projekt2_dialog_base.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Projekt2_dialog_base.ui'
+# Form implementation generated from reading ui file '.\Projekt2_dialog_base.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.9
 #
@@ -85,6 +85,12 @@ class Ui_Projekt2DialogBase(object):
         self.pushButton_porow = QtWidgets.QPushButton(Projekt2DialogBase)
         self.pushButton_porow.setGeometry(QtCore.QRect(400, 140, 161, 31))
         self.pushButton_porow.setObjectName("pushButton_porow")
+        self.pushButton_explore = QtWidgets.QPushButton(Projekt2DialogBase)
+        self.pushButton_explore.setGeometry(QtCore.QRect(190, 190, 31, 31))
+        self.pushButton_explore.setObjectName("pushButton_explore")
+        self.lineEdit_sciezka = QtWidgets.QLineEdit(Projekt2DialogBase)
+        self.lineEdit_sciezka.setGeometry(QtCore.QRect(30, 190, 161, 31))
+        self.lineEdit_sciezka.setObjectName("lineEdit_sciezka")
 
         self.retranslateUi(Projekt2DialogBase)
         self.button_box.accepted.connect(Projekt2DialogBase.accept) # type: ignore
@@ -108,6 +114,7 @@ class Ui_Projekt2DialogBase(object):
         self.pushButton_clear.setText(_translate("Projekt2DialogBase", "Wyczyść"))
         self.pushButton_rysuj.setText(_translate("Projekt2DialogBase", "Rysuj poligon"))
         self.pushButton_porow.setText(_translate("Projekt2DialogBase", "Porównaj poligon"))
+        self.pushButton_explore.setText(_translate("Projekt2DialogBase", "..."))
 from qgsmaplayercombobox import QgsMapLayerComboBox
 
 
